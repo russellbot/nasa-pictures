0e7125321276f1dfef92eb12aa18b19961b76cdc
Russell Hart <russellbot@gmail.com>
2020-10-28 10:42:35 -0400

    save favorites in localStorage

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const apiKey = 'DEMO_KEY';
 const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
 
 let resultsArray = [];
+let favorites = {};
 
 function updateDOM() {
     resultsArray.forEach((result) => {
@@ -38,6 +39,7 @@ function updateDOM() {
         const saveText = document.createElement('p');
         saveText.classList.add('clickable');
         saveText.textContent = 'Add To Favorites';
+        saveText.setAttribute('onclick', `saveFavorite('${result.url}')`);
         // Card Text
         const cardText = document.createElement('p');
         cardText.classList.add('card-text');
@@ -71,6 +73,23 @@ async function getNasaPictures() {
     } catch (error) {
         // Catch Error Here
     }
+}
+
+// Add result to favorites
+function saveFavorite(itemUrl) {
+    // Loop through Results array to select favorite
+    resultsArray.forEach((item) => {
+        if (item.url.includes(itemUrl) && !favorites[itemUrl]) {
+            favorites[itemUrl] = item;
+            // Show Save Confirmation for 2 seconds
+            saveConfirmed.hidden = false;
+            setTimeout(() => {
+                saveConfirmed.hidden = true;
+            }, 2000);
+            // Set Favorites in localStorage
+            localStorage.setItem('nasaFavorites', JSON.stringify(favorites));
+        }
+    });
 }
 
 // On Load
